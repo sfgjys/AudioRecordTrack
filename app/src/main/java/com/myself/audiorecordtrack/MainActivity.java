19dc76d24bb85ef462aad99d6d1939bd1f926537
sfgjys <sfgjys@sina.com>
2017-09-05 11:23:00 +0800

    整理边录边播代码

--- app/src/main/java/com/myself/audiorecordtrack/MainActivity.java
+++ app/src/main/java/com/myself/audiorecordtrack/MainActivity.java
@@ -49,10 +49,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private byte[] mPlayByte;
     /**
-     * 变量的描述:
+     * 变量的描述: 在采集音频时用来存储两个缓存byte[]，当播放时可直接使用第一个缓存byte[]（第一个缓存byte[]，只有在获取到第三个缓存byte[]时，才会去除掉）
      */
     private LinkedList<byte[]> mListByte;
 
+    // --------------------------------------------------------------------------------------------------------
+
+    /**
+     * 变量的描述: 音频采集频率
+     */
+    private final int mSamplingFrequency = 32000;
+    /**
+     * 变量的描述: 声道类型(单声道或者双声道) AudioFormat.CHANNEL_IN_STEREO 和 AudioFormat.CHANNEL_OUT_STEREO 值一样
+     */
+    private final int mChannelType = AudioFormat.CHANNEL_IN_STEREO;
+    /**
+     * 变量的描述: 采集音频的比特率
+     */
+    private final int mBitRate = AudioFormat.ENCODING_PCM_16BIT;
+    /**
+     * 变量的描述: 录音的声源
+     */
+    private final int mRecordingSoundSource = MediaRecorder.AudioSource.MIC;
+    /**
+     * 变量的描述: 音频流的类型  STREAM_VOICE_CALL, STREAM_SYSTEM, STREAM_RING, STREAM_MUSIC, STREAM_ALARM, STREAM_NOTIFICATION.
+     */
+    private final int mAudioStreamType = AudioManager.STREAM_MUSIC;
+    /**
+     * 变量的描述: 播放的数据类型 流或者静态的缓冲区 播放是实时的还是静态的数据
+     */
+    private final int mPlayDataType = AudioTrack.MODE_STREAM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,36 +87,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initView();
 
-        initTool();
-
         initThread();
-
     }
 
     private void initTool() {
         // AudioRecord 得到录制最小缓冲区的大小
-        mCollectMinBufferSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        mCollectMinBufferSize = AudioRecord.getMinBufferSize(mSamplingFrequency, mChannelType, mBitRate);
         // 实例化采集麦克风收集的PCM数据的工具
-        mCollectPCMTool = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, mCollectMinBufferSize);
-
-        // --------------------------------------------------------------------------------------------------------
-
-        // 实例化一个字节数组，长度为最小缓冲区的长度
-        mCollectByte = new byte[mCollectMinBufferSize];
-        // 实例化一个链表，用来存放字节组数
-        mListByte = new LinkedList<byte[]>();
+        mCollectPCMTool = new AudioRecord(mRecordingSoundSource, mSamplingFrequency, mChannelType, mBitRate, mCollectMinBufferSize);
 
         // --------------------------------------------------------------------------------------------------------
 
         // AudioTrack 得到播放最小缓冲区的大小
-        mPlayMinBufferSize = AudioTrack.getMinBufferSize(8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        mPlayMinBufferSize = AudioTrack.getMinBufferSize(mSamplingFrequency, mChannelType, mBitRate);
         // 实例化播放音频对象
-        mPlayPCMTool = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, mPlayMinBufferSize, AudioTrack.MODE_STREAM);
-
-        // --------------------------------------------------------------------------------------------------------
-
-        // 实例化一个长度为播放最小缓冲大小的字节数组
-        mPlayByte = new byte[mPlayMinBufferSize];
+        mPlayPCMTool = new AudioTrack(mAudioStreamType, mSamplingFrequency, mChannelType, mBitRate, mPlayMinBufferSize, mPlayDataType);
     }
 
     private void initView() {
@@ -103,10 +114,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mPlayPCMThread = new Thread(new PlayPCM());
     }
 
+    private void initDataSave() {
+
+        // 实例化一个字节数组，长度为最小缓冲区的长度
+        mCollectByte = new byte[mCollectMinBufferSize];
+        // 实例化一个链表，用来存放字节组数
+        mListByte = new LinkedList<>();
+
+        // --------------------------------------------------------------------------------------------------------
+
+        // 实例化一个长度为播放最小缓冲大小的字节数组
+        mPlayByte = new byte[mPlayMinBufferSize];
+    }
+
+    private void clearData() {
+        if (mListByte != null) {
+            mListByte.clear();
+            mListByte = null;
+        }
+        mCollectByte = null;
+        mPlayByte = null;
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.start:
+                initTool();
+                initDataSave();
                 mThreadRoutineIsContinue = true;
 
                 // 启动采集PCM线程
@@ -120,6 +155,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mCollectPCMTool = null;
                 mPlayPCMTool.stop();
                 mPlayPCMTool = null;
+                clearData();
                 break;
         }
     }
@@ -127,17 +163,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private class CollectPCM implements Runnable {
         @Override
         public void run() {
-            byte[] bytes_pkg;
+            // 在mCollectByte获取到了新的录音数据缓存时，克隆出一个byte[]给collectMiddleBytes，然后将collectMiddleBytes放入链表集合中供播放使用
+            byte[] collectMiddleBytes;
             // 开始录音
             mCollectPCMTool.startRecording();
 
             while (mThreadRoutineIsContinue) {
                 mCollectPCMTool.read(mCollectByte, 0, mCollectMinBufferSize);
-                bytes_pkg = mCollectByte.clone();
+                collectMiddleBytes = mCollectByte.clone();
                 if (mListByte.size() >= 2) {
                     mListByte.removeFirst();
                 }
-                mListByte.add(bytes_pkg);
+                mListByte.add(collectMiddleBytes);
             }
         }
     }
@@ -145,15 +182,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private class PlayPCM implements Runnable {
         @Override
         public void run() {
-            byte[] bytes_pkg = null;
+            byte[] playMiddle;
             // 开始播放
             mPlayPCMTool.play();
 
             while (mThreadRoutineIsContinue) {
                 try {
                     mPlayByte = mListByte.getFirst();
-                    bytes_pkg = mPlayByte.clone();
-                    mPlayPCMTool.write(bytes_pkg, 0, bytes_pkg.length);
+                    playMiddle = mPlayByte.clone();
+                    mPlayPCMTool.write(playMiddle, 0, playMiddle.length);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
